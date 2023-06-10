c1e06a6dccb422b2665aa81120568a3afb39ce93
zigzagdev <msts.oo0131@gmail.com>
2023-06-10 19:18:01 +0900

    tailwindCSS_import

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Main from './Main'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+const tailwindcss = require('tailwindcss');
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'xxs': {'min': '1px', 'max': '367px'},
+      'xs': {'min': '368px', 'max': '639px'},
+      'sm': {'min': '640px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1535px'},
+      '2xl': {'min': '1536px'},
+    }, extend: {
+      'xxs': {'min': '1px', 'max': '367px'},
+      'xs': {'min': '368px', 'max': '639px'},
+    }
+  },
+  plugins: [],
+}
+
