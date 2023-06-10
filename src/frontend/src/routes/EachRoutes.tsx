9c03510a3709b85e0cb0cebb1cbe3055fa1277c6
zigzagdev <msts.oo0131@gmail.com>
2023-06-11 00:21:30 +0900

    temporaryAddFile

--- src/frontend/src/routes/EachRoutes.tsx
+++ src/frontend/src/routes/EachRoutes.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Footer from "../components/common/Footer";
-import Header from "../components/common/Header";
-import Test from "../components/app/Test";
-import Index from "../components/Index";
 import {
     BrowserRouter,
     Routes,
     Route,
 } from "react-router-dom";
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import Test from "../components/app/Test";
+import Index from "../components/Index";
+import Instagram from "../components/common/footer/Instagram";
+
 
 
 export default function EachRoutes() {
@@ -20,6 +22,7 @@ export default function EachRoutes() {
                         <Route path="/Header" element={<Header/>} />
                         <Route path="/Footer" element={<Footer/>} />
                         <Route path="/Test" element={<Test/>} />
+                        <Route path="/Instagram" element={<Instagram/>} />
                     </Routes>
                 </BrowserRouter>
             </div>
