31e14197b35f73029243506f2a7baa9ba1009718
zigzagdev <msts.oo0131@gmail.com>
2023-06-12 23:00:54 +0900

    fixMigrationColumnType

--- src/backend/database/migrations/2023_06_12_011117_create_items_table.php
+++ src/backend/database/migrations/2023_06_12_011117_create_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -21,7 +20,7 @@ return new class extends Migration
             $table->tinyInteger('category')->unsigned();
             $table->integer('click_count');
             $table->integer('admin_id')->unsigned();
-            $table->foreign('admin_id')->references('id')->on('admins')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('admins')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
         });
