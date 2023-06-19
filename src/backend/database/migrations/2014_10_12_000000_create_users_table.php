295474c28b53842f374b59daa05d365cd0f2cbaf
zigzagdev <msts.oo0131@gmail.com>
2023-06-19 23:47:27 +0900

    fixMigrationColumnsValidationSetting

--- src/backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
+            $table->string('password', 255)->unique();
             $table->timestamps();
             $table->softDeletes();
         });
