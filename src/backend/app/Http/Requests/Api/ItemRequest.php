4f08b8f30960e36d658362a1980320ffc9ca123d
zigzagdev <msts.oo0131@gmail.com>
2023-06-23 01:16:50 +0900

    itemRequestValidationAdd

---
+++ src/backend/app/Http/Requests/Api/ItemRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+class ItemRequest extends CommonRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "name" => "required|min:5||max:100|string",
+            "description" => "required|min:4|max:100|string",
+            "price" => "required|integer",
+            "category" => "required|integer",
+            "click_count" => "required|integer",
+        ];
+    }
+
+    public function messages()
+    {
+        $message = $this->errorMessages();
+        return  [
+            //name
+            "name.required" => $message['required'],
+            "name.min" => sprintf($message['min'], 4),
+            "name.max" => sprintf($message['min'], 100),
+
+            //description
+            "description.required" => $message['required'],
+            "description.min" => sprintf($message['min'], 8),
+            "description.max" => sprintf($message['max'], 255),
+
+            //price
+            "price.required" => $message['required'],
+            "price.integer" => $message['integer'],
+
+            //category
+            "category.required" => $message['required'],
+            "category.integer" => $message['integer'],
+
+            //click_count
+            "click_count.required" => $message['required'],
+            "click_count.integer" => $message['integer'],
+        ];
+    }
+}
