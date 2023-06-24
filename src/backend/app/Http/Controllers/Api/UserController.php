0921d0ccdded2d1650668da93132e9df0b8dd5a2
zigzagdev <msts.oo0131@gmail.com>
2023-06-24 16:50:34 +0900

    getAllUserParamms

--- src/backend/app/Http/Controllers/Api/UserController.php
+++ src/backend/app/Http/Controllers/Api/UserController.php
@@ -7,12 +7,13 @@ use App\Consts\Common;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\UserRequest;
 use App\Http\Resources\Api\ErrorResource;
+use App\Http\Resources\Api\UserAllCollection;
 use App\Http\Resources\Api\UserResource;
 use App\Models\Api\User;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Mockery\Exception;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
@@ -69,4 +70,31 @@ class UserController extends Controller
             DB::rollBack();
         }
     }
+
+    public function allUser(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $users = User::all();
+
+
+            if (empty($users)) {
+                $request->merge(['statusMessage' => sprintf(Common::ERR_05)]);
+                $statusCode = Message::Unauthorized;
+
+                return new ErrorResource($request, $statusCode);
+            }
+
+            return new UserAllCollection($users);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            $request->merge(['statusMessage' => sprintf(Common::FETCH_FAILED, 'ユーザーデータ')]);
+            $statusMessage = $e->getMessage();
+
+            return new ErrorResource($request, Response::HTTP_BAD_REQUEST);
+        }
+    }
+
 }
