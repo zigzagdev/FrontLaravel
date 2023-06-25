f051344d69d85de91d7df61358f755196a1ff30a
zigzagdev <msts.oo0131@gmail.com>
2023-06-25 13:38:23 +0900

    changeFileName

--- src/frontend/src/components/app/post/PostSlug.tsx
+++ src/frontend/src/components/app/post/PostSlug.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../../common/header/Header";
 import Footer from "../../common/footer/Footer";
 
-export default function Slug() {
+export default function PostSlug() {
     return(
         <>
             <Header/>
