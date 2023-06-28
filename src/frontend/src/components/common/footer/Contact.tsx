64b78b52470f8c979c599ce1e2c494e168efbc8d
zigzagdev <msts.oo0131@gmail.com>
2023-06-29 01:35:42 +0900

    addGoogleForm

--- src/frontend/src/components/common/footer/Contact.tsx
+++ src/frontend/src/components/common/footer/Contact.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Contact() {
     return (
         <>
             <div className='my-2'>
-                testhere
+                <iframe
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSe5jvqenZfc625oGMX5KYy1zd322z47Zsts1JuwumjZHQm33A/viewform?usp=sf_link"
+                    width="100%"
+                    height="800"
+                >
+                    Now Loading .....
+                </iframe>
             </div>
         </>
     )
