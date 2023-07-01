6eca0e4f738c367dc9a1163886ed2c54c7b562f7
zigzagdev <msts.oo0131@gmail.com>
2023-07-01 13:55:02 +0900

    addConstCategoryFile

---
+++ src/backend/app/Consts/Api/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Consts\Api;
+
+class Category{
+
+    // each select category
+    public const Food = 1;
+    public const Soft_Drink = 2;
+    public const Device = 3;
+    public const Audio = 4;
+    public const Application = 5;
+    public const Book = 6;
+    public const Foreign_Book = 7;
+    public const Classic = 8;
+    public const DVD = 9;
+    public const TV_Game = 10;
+    public const PC = 11;
+    public const Camera = 12;
+    public const Stationary = 13;
+    public const DIY = 14;
+    public const Pets = 15;
+    public const Drug_Store = 16;
+    public const Beauty = 17;
+    public const Mens = 18;
+    public const Women = 19;
+    public const Baby = 20;
+    public const Fusion = 21;
+    public const Toy = 22;
+    public const Hobby = 23;
+    public const Instrument = 24;
+    public const Sports = 25;
+    public const Car_and_Motor_Bike = 25;
+    public const Large_Appliances = 26;
+    public const Gift_Card = 27;
+}
+
+
