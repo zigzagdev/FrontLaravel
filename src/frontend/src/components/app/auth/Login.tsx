d59b0358f99cb22d2de1aef80563236bec7fb419
zigzagdev <msts.oo0131@gmail.com>
2023-07-08 14:41:43 +0900

    loginFormFrontend

--- src/frontend/src/components/app/auth/Login.tsx
+++ src/frontend/src/components/app/auth/Login.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Login() {
+    const [mail, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
+
     return(
         <>
-          Login here
+          
         </>
     )
 }
