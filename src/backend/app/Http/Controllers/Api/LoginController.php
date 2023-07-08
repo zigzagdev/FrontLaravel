1fee7afa8ea294288e5365e434c6c8788889f6d1
zigzagdev <msts.oo0131@gmail.com>
2023-07-08 19:34:01 +0900

    loginFormCreated

---
+++ src/backend/app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Consts\Api\Message;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\ErrorResource;
+use App\Http\Resources\Api\LoginResource;
+use App\Models\Api\Admin;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class LoginController extends Controller
+{
+    function loginAuth(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $authentication = Admin::where('email', $request->email)->first();
+
+            if (!$authentication) {
+                $request->merge(['statusMessage' => Message::No_Content]);
+                return new ErrorResource($request, Response::HTTP_UNAUTHORIZED);
+            }
+
+            $password = $request->password;
+            if (!Hash::check($password, $authentication->password)) {
+                $request->merge(['statusMessage' => Message::Unauthorized]);
+                return new ErrorResource($request);
+            }
+
+            DB::commit();
+
+            return new LoginResource($request);
+        } catch (\Exception $e) {
+
+        }
+    }
+}
