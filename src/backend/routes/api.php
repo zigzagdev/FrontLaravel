ae1d4340b0ba23ea6ba6990e04a5da7e0956c360
zigzagdev <msts.oo0131@gmail.com>
2023-07-09 12:32:56 +0900

    apiRouteFix

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AdminController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ItemController;
+use App\Http\Controllers\Api\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,11 @@ use App\Http\Controllers\Api\ItemController;
 */
 
 Route::middleware(['api', 'cors'])->group(function () {
+
+    Route::controller(AuthenticationController::class)->group(function () {
+        Route::post('/login', 'loginAuth');
+        Route::post('/logout', 'logoutAuth');
+    });
 
     Route::controller(AdminController::class)->group(function () {
         Route::post('/registerAdmin', 'createAdmin');
