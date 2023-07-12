6c472b2a5b419599659d637875b466cd4d409f04
zigzagdev <msts.oo0131@gmail.com>
2023-07-13 00:46:48 +0900

    addCSVFile

---
+++ src/backend/app/Models/Api/Csv.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Api;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Csv extends Model
+{
+    use HasFactory;
+
+    protected $model = Item::class;
+
+}
