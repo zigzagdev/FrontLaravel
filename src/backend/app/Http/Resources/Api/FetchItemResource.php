0790e1eedc9460021180149abb6397926c1218fa
zigzagdev <msts.oo0131@gmail.com>
2023-09-03 22:09:22 +0900

    changeAPIResourceFile

---
+++ src/backend/app/Http/Resources/Api/FetchItemResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FetchItemResource extends JsonResource
+{
+    public function __construct($request, $statusCode = 200)
+    {
+        parent::__construct($request);
+        $this->statusCode = $statusCode;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'statusCode' => $this->statusCode,
+            'statusMessage' => 'OK',
+            'profile' => [
+                'itemName' => $this->name,
+                'content' => $this->description,
+                'price' => number_format($this->price),
+                'slug' => $this->slug,
+                'category' => $this->categoryName,
+            ]
+        ];
+    }
+}
