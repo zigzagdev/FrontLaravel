2a7c0593a86128985189406e8fa9b928ed7530b1
zigzagdev <msts.oo0131@gmail.com>
2023-09-03 14:49:23 +0900

    eachPostProperty_appeared

--- src/frontend/src/components/app/post/PostSlug.tsx
+++ src/frontend/src/components/app/post/PostSlug.tsx
@@ -1,13 +1,33 @@
 import React, {useEffect, useState} from "react";
-import axios from "axios/index";
+import axios from "axios";
+import {useParams} from "react-router-dom";
+
+interface ItemInfo {
+    id?: number,
+    itemName?: string,
+    content?: string,
+    price?: number,
+    categoryName?: string,
+    slug?: string
+}
 
 
-export default function PostSlug() {
-    console.log()
+export function PostSlug() {
+    const {slug} = useParams<{slug: string}>();
+    const [item, setItem] = useState<ItemInfo>();
+    const baseURL = process.env.REACT_APP_API_BASE_URL;
+    useEffect(() => {
+        axios.get(`${baseURL}./display/`+slug)
+            .then(res => {
+                setItem(res.data.data.profile)
+            })
+    }, [])
+    console.log(item)
     return (
         <>
+            {slug}
             <div>
-
+                {item?.itemName}
             </div>
 
         </>
