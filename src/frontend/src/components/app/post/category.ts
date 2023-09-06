d3ba109188bff6d812363110a12fe26557f5620d
zigzagdev <msts.oo0131@gmail.com>
2023-09-06 23:59:17 +0900

    addCategoryFrontFile

---
+++ src/frontend/src/components/app/post/category.ts
@@ -0,0 +1,37 @@
+
+export interface Category {
+    id: number,
+    label: string,
+    value: string
+}
+
+export const Category: Category[] = [
+    { id: 1, label: "Food", value: "1" },
+    { id: 2, label: "Gift_Card", value: "2" },
+    { id: 3, label: "Large_Appliances", value: "3" },
+    { id: 4, label: "Sports`", value: "4" },
+    { id: 5, label: "Instrument", value: "5" },
+    { id: 6, label: "Hobby", value: "6" },
+    { id: 7, label: "Toy", value: "7" },
+    { id: 8, label: "Fusion", value: "8" },
+    { id: 9, label: "Baby", value: "9" },
+    { id: 10, label: "Women", value: "10" },
+    { id: 11, label: "Mens", value: "11" },
+    { id: 12, label: "Pets", value: "12" },
+    { id: 13, label: "DIY", value: "13" },
+    { id: 14, label: "Stationary", value: "14" },
+    { id: 15, label: "Camera", value: "15" },
+    { id: 16, label: "PC", value: "16" },
+    { id: 17, label: "TV_Game", value: "17" },
+    { id: 18, label: "DVD", value: "18" },
+    { id: 19, label: "Classic", value: "19" },
+    { id: 20, label: "Foreign_Book", value: "20" },
+    { id: 21, label: "Book", value: "21" },
+    { id: 22, label: "Application", value: "22" },
+    { id: 23, label: "Audio", value: "23" },
+    { id: 24, label: "Device", value: "24" },
+    { id: 25, label: "Soft_Drink", value: "25" },
+    { id: 26, label: "Car_and_Motor_Bike", value: "26" },
+    { id: 27, label: "Beauty", value: "27" },
+    { id: 28, label: "Drug_Store", value: "28" },
+];
