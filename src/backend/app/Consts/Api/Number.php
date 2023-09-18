91bfcf6f07ce2d10ea8c12edb2b853a22074f9cc
zigzagdev <msts.oo0131@gmail.com>
2023-09-18 19:28:51 +0900

    addConstNumber

--- src/backend/app/Consts/Api/Number.php
+++ src/backend/app/Consts/Api/Number.php
@@ -13,4 +13,11 @@ class Number
 
     public const Display_Flag = 1;
     public const Expired_Flag = 2;
+
+    public const One_Day = 1;
+    public const Two_Days = 2;
+    public const Three_Days = 3;
+    public const Four_Days = 4;
+    public const Five_Days = 5;
+    public const Six_Days = 6;
 }
