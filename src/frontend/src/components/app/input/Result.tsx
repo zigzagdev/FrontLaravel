645ce8c2608a3d5b2ccb3ecb01beef42fb43d87e
zigzagdev <msts.oo0131@gmail.com>
2023-09-18 19:05:13 +0900

    searchIndex_fixed

--- src/frontend/src/components/app/input/Result.tsx
+++ src/frontend/src/components/app/input/Result.tsx
@@ -1,25 +1,26 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-interface searchResults {
-    id?: number,
-    itemName?: string,
-    content?: string,
-    price?: number,
-    categoryName?: string
+type searchResults = {
+    Id: number,
+    itemName: string,
+    content: string,
+    price: number,
+    categoryName: string
 }
 
 
 export default function Result() {
+    const queryParams = new URLSearchParams(window.location.search)
+    const query = queryParams.get("q")
     const [searchContents, setSearchContents] = useState<searchResults[]>([]);
     const baseURL = process.env.REACT_APP_API_BASE_URL;
     useEffect(() => {
-        axios.get(`${baseURL}./search?q=${searchContents}`)
+        axios.get(`${baseURL}./search?q=${query}`)
             .then(res => {
                 setSearchContents(res.data.data.itemInformation)
             })
     }, [])
-
     return (
         <>
             <div className="my-3 text-center block text-sm text-gray-300 sm:text-center
--- src/frontend/src/components/app/input/Search.tsx
+++ src/frontend/src/components/app/input/Search.tsx
@@ -4,14 +4,16 @@ import NotFound from "../exception/NotFound";
 import {useNavigate} from "react-router-dom";
 
 export default function Search() {
+    const [query, setQuery] = useState('');
     const [searchContent, setSearchContent] = useState("");
     const baseURL = process.env.REACT_APP_API_BASE_URL;
     const navigate = useNavigate();
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const {data} = await axios.get(
-                    `${baseURL}./search?q=${searchContent}`
+                    `${baseURL}./search?q=${query}`
                 );
                 setSearchContent(data.products);
             } catch (error) {
@@ -29,15 +31,15 @@ export default function Search() {
                             type="search"
                             placeholder=" Search Something ..."
                             className=" w-1/4 h-8 outline-double mx-4 text-black indent-4"
-                            value={searchContent}
+                            value={query}
                             onChange={(e) => {
-                                setSearchContent(e.target.value);
+                                setQuery(e.target.value);
                             }}
                         />
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
                             onClick={() => {
-                                navigate(`/Result?q=${searchContent}`)
+                                navigate(`/Result?q=${query}`)
                             }}
                         >
                             Search
