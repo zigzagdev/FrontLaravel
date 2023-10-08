5addf7c14eff9a963805aae5eb45306045c84f16
zigzagdev <msts.oo0131@gmail.com>
2023-10-08 17:09:36 +0900

    Admin-Name-Updated

--- src/frontend/src/components/app/admin/Admin.tsx
+++ src/frontend/src/components/app/admin/Admin.tsx
@@ -9,6 +9,16 @@ type adminData = {
     email: string,
 }
 
+type emailData = {
+    id: number,
+    email: string,
+}
+
+type nameData = {
+    id: number,
+    name: string,
+}
+
 // export function CreateAdmin() {
 //
 // }
@@ -30,46 +40,103 @@ export function AdminData() {
         <>
             <div className="my-4 mx-32 block text-lg duration-700">
                 <div>{adminData.name}</div>
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white
+                               font-bold py-2 px-4 border border-blue-700 rounded">
+                    <a href="/Admin/Update/Name">Name</a>
+                </button>
             </div>
             <div className="my-4 mx-32 block text-lg duration-700">
                 <div>{adminData.email}</div>
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white
+                               font-bold py-2 px-4 border border-blue-700 rounded">
+                    <a href="/Admin/Update/Email">Email</a>
+                </button>
             </div>
         </>
     )
 }
 
 export function UpdateName() {
+    const [AdminName, setAdminName] = useState<nameData>({
+        id: 0,
+        name: "",
+    });
+    const baseURL = process.env.REACT_APP_API_BASE_URL;
+    const navigate = useNavigate();
+    const [error, setError] = useState('');
+    const {register, handleSubmit, formState: {errors}} = useForm<nameData>();
+    const onSubmit: SubmitHandler<nameData> = (data: nameData) => {
+        axios
+            .put<nameData>(`${baseURL}./admin/update`, {
+                name: AdminName.name,
+            })
+            .then((res) => {
+                return (
+                    navigate('/Admin')
+                )
+            })
+            .catch((error: any) => {
+                if (error.response.statusText == 'Bad Request') {
+                    setError('Item Information could not updated ...');
+                } else {
+                    setError('Internal server error is happened. Please do it again.');
+                }
+            })
+    };
+    useEffect(() => {
+        axios.get(`${baseURL}./admin/profile`)
+            .then(res => {
+                setAdminName(res.data.data.profile)
+            })
+    }, [])
     return (
         <>
-            <div>
-                update admin name
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="my-4 mx-32 block text-lg duration-700">
+                    <input
+                        type="name"
+                        placeholder=""
+                        value={AdminName.name}
+                        {...register("name", {required: true, minLength: 4, maxLength: 100})}
+                        onChange={(e) => setAdminName({...AdminName, name: e.target.value})}
+                    />
+                    <div className="my-10 mx-5 text-red-600 font-mono text-lg">
+                        {error}
+                    </div>
+                </div>
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                    type="submit"
+                >
+                    Submit
+                </button>
+            </form>
         </>
     )
 }
 
 export function UpdateEmail() {
-    const [AdminData, setAdminData] = useState<adminData>({
+    const [AdminEmail, setAdminEmail] = useState<emailData>({
         id: 0,
-        name: "",
         email: "",
     });
     const baseURL = process.env.REACT_APP_API_BASE_URL;
     const navigate = useNavigate();
     const [error, setError] = useState('');
-    const {register, handleSubmit, formState: {errors}} = useForm<adminData>()
+    const {register, handleSubmit, formState: {errors}} = useForm<emailData>()
     useEffect(() => {
         axios.get(`${baseURL}./admin/profile`)
             .then(res => {
-                setAdminData(res.data.data.profile)
+                setAdminEmail(res.data.data.profile)
             })
     }, [])
 
-    const onSubmit: SubmitHandler<adminData> = (data: adminData) => {
+    const onSubmit: SubmitHandler<emailData> = (data: emailData) => {
         axios
-            .put<adminData>(`${baseURL}./admin/update/email`, {
-                email: AdminData.email,
-                name: AdminData.name
+            .put<emailData>(`${baseURL}./admin/update/email`, {
+                email: AdminEmail.email,
             })
             .then((res) => {
                 return (
@@ -88,15 +155,12 @@ export function UpdateEmail() {
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="my-4 mx-32 block text-lg duration-700">
-                    {AdminData.name}
-                </div>
-                <div className="my-4 mx-32 block text-lg duration-700">
                     <input
                         type="email"
                         placeholder=""
-                        value={AdminData.email}
+                        value={AdminEmail.email}
                         {...register("email", {required: true, minLength: 4, maxLength: 100})}
-                        onChange={(e) => setAdminData({...AdminData, email: e.target.value})}
+                        onChange={(e) => setAdminEmail({...AdminEmail, email: e.target.value})}
                     />
                     {errors.email?.type === "required" && (
                         <p role="alert" className="text-red-300">ItemName is required</p>
@@ -124,5 +188,6 @@ export function UpdateEmail() {
 
 
 // export function DeleteAdmin() {
+
 //
 // }
