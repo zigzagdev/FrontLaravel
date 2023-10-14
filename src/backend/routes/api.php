bdbc09200e2f68c554b39f13cf36db7a5f5f320d
zigzagdev <msts.oo0131@gmail.com>
2023-10-14 15:43:09 +0900

    Routing-fix

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -27,8 +27,8 @@ Route::middleware(['api', 'cors'])->group(function () {
     // Only user can see the page .
     Route::controller(AdminController::class)->group(function () {
         Route::post('/admin/create', 'createAdmin');
-        Route::put('/admin/update/name', 'updateAdminName');
         Route::get('/admin/profile', 'getAdmin');
+        Route::put('/admin/update/name', 'updateAdminName');
         Route::put('/admin/update/email', 'updateAdminEmail');
         Route::get('/admin/user/all', 'allUser');
     });
@@ -37,16 +37,16 @@ Route::middleware(['api', 'cors'])->group(function () {
         Route::post('/user/create', 'createUser');
         Route::get('/user/{id}', 'eachUser');
         Route::put('/user/{id}/update/name', 'updateUserName');
-        Route::put('/user/update/email', 'updateEmail');
+        Route::put('/user/{id}/update/email', 'updateUserEmail');
     });
 
     Route::controller(ItemController::class)->group(function () {
         Route::post('/create/item', 'createItem');
         // first display URL
         Route::get('/items', 'allItems');
-        Route::get('/display/{slug}', 'displayDetail');
+        Route::get('/item/{slug}', 'displayItem');
+        Route::post('/item/{slug}', 'deleteItem');
+        Route::put('/item/{slug}/update', 'updateItem');
         Route::get('/search', 'searchItems');
-        Route::put('/update/{slug}', 'updateItem');
-        Route::post('/item/{id}', 'deleteItem');
     });
 });
