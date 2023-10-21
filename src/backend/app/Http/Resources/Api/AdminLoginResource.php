667e1c387379d6b5cb8893016ea9d229a96205f2
zigzagdev <msts.oo0131@gmail.com>
2023-10-21 17:31:55 +0900

    separating-login-return

---
+++ src/backend/app/Http/Resources/Api/AdminLoginResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AdminLoginResource extends JsonResource
+{
+
+    public function __construct($request, $statusCode = 200)
+    {
+        parent::__construct($request);
+        $this->statusCode = $statusCode;
+    }
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'statusCode' => '200',
+            'statusMessage' => 'OK',
+        ];
+    }
+}
--- src/backend/app/Http/Resources/Api/UserLoginResource.php
+++ src/backend/app/Http/Resources/Api/UserLoginResource.php
@@ -4,26 +4,19 @@ namespace App\Http\Resources\Api;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class LoginResource extends JsonResource
+class UserLoginResource extends JsonResource
 {
-
-    public function __construct($request, $statusCode = 200)
-    {
-        parent::__construct($request);
-        $this->statusCode = $statusCode;
-    }
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
             'statusCode' => '200',
-            'statusMessage' => 'OK',
-            'expiration' => $this->expiration
+            'statusMessage' => 'Hello'.$this->name. '!'
         ];
     }
 }
