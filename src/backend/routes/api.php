20a3d8c13da73ca2a74ade844727e3dd2233951c
zigzagdev <msts.oo0131@gmail.com>
2023-10-29 00:43:29 +0900

    Message-fix

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -17,36 +17,34 @@ use App\Http\Controllers\Api\AuthenticationController;
 |
 */
 
-Route::middleware(['api', 'cors'])->group(function () {
+Route::controller(AuthenticationController::class)->group(function () {
+    Route::post('/admin/login', 'adminLogin');
+    Route::post('/login', 'userLogin');
+    Route::post('/logout', 'logoutAction');
+});
 
-    Route::controller(AuthenticationController::class)->group(function () {
-        Route::post('/login', 'loginAuth');
-        Route::post('/logout', 'logoutAuth');
-    });
+// Only user can see the page .
+Route::controller(AdminController::class)->group(function () {
+    Route::post('/admin/create', 'createAdmin');
+    Route::get('/admin/profile', 'getAdmin');
+    Route::put('/admin/update/name', 'updateAdminName');
+    Route::put('/admin/update/email', 'updateAdminEmail');
+    Route::get('/admin/user/all', 'allUser');
+});
 
-    // Only user can see the page .
-    Route::controller(AdminController::class)->group(function () {
-        Route::post('/admin/create', 'createAdmin');
-        Route::get('/admin/profile', 'getAdmin');
-        Route::put('/admin/update/name', 'updateAdminName');
-        Route::put('/admin/update/email', 'updateAdminEmail');
-        Route::get('/admin/user/all', 'allUser');
-    });
+Route::controller(UserController::class)->group(function () {
+    Route::post('/user/create', 'createUser');
+    Route::get('/user/{id}', 'eachUser');
+    Route::put('/user/{id}/update/name', 'updateUserName');
+    Route::put('/user/{id}/update/email', 'updateUserEmail');
+});
 
-    Route::controller(UserController::class)->group(function () {
-        Route::post('/user/create', 'createUser');
-        Route::get('/user/{id}', 'eachUser');
-        Route::put('/user/{id}/update/name', 'updateUserName');
-        Route::put('/user/{id}/update/email', 'updateUserEmail');
-    });
-
-    Route::controller(ItemController::class)->group(function () {
-        Route::post('/create/item', 'createItem');
-        // first display URL
-        Route::get('/items', 'allItems');
-        Route::get('/item/{slug}', 'displayItem');
-        Route::post('/item/{slug}', 'deleteItem');
-        Route::put('/item/{slug}/update', 'updateItem');
-        Route::get('/search', 'searchItems');
-    });
+Route::controller(ItemController::class)->group(function () {
+    Route::post('/create/item', 'createItem');
+    // first display URL
+    Route::get('/items', 'allItems');
+    Route::get('/item/{slug}', 'displayItem');
+    Route::post('/item/{slug}', 'deleteItem');
+    Route::put('/item/{slug}/update', 'updateItem');
+    Route::get('/search', 'searchItems');
 });
