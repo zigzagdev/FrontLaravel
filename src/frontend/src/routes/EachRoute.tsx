4eb918bb66b02ee600524201da9394d63976b559
zigzagdev <msts.oo0131@gmail.com>
2023-11-16 00:00:26 +0900

    header&footer-fix

--- src/frontend/src/routes/EachRoute.tsx
+++ src/frontend/src/routes/EachRoute.tsx
@@ -4,25 +4,22 @@ import {
     Routes,
     Route,
 } from "react-router-dom";
-import Footer from "../components/common/footer/Footer";
-import Header from "../components/common/header/Header";
 import {Posts} from "../components/app/post/Posts";
 import {TopPage} from "../components/TopPage";
 import Instagram from "../components/common/footer/Instagram";
 import NotFound from "../components/app/exception/NotFound";
 import Contact from "../components/common/footer/Contact";
 import AdminLogin from "../components/app/auth/AdminLogin";
+import UserLogin from "../components/app/auth/UserLogin";
 import Result from "../components/app/input/Result";
 import {ShowSlug, EditSlug} from "../components/app/post/PostSlug";
 import {AdminData, AllUsers, EditAdminEmail, EditAdminName} from "../components/app/admin/Admin";
-import {EachUserData, UpdateUserEmail, UpdateUserName} from "../components/app/user/Info";
-import UserLogin from "../components/app/auth/UserLogin";
+import {EachUserData, EditUserEmail, EditUserName} from "../components/app/user/Info";
 
 
 export default function EachRoute() {
     return (
         <BrowserRouter>
-            <Header/>
             <Routes>
                 <Route path="/Admin/Login" element={<AdminLogin/>}/>
                 <Route path="/Login" element={<UserLogin/>}/>
@@ -33,7 +30,7 @@ export default function EachRoute() {
                 <Route path="/Result" element={<Result/>}/>
                 <Route path="*" element={<NotFound/>}/>
                 //AdminProvider
-                <Route path="/Admin/Profile" element={<AdminData/>}/>
+                <Route path="/Admin/:id/Profile" element={<AdminData/>}/>
                 <Route path="/Post/:slug" element={<ShowSlug/>}/>
                 <Route path="/Post/:slug/Edit" element={<EditSlug/>}/>
                 <Route path="Admin/Update/Email" element={<EditAdminName/>}/>
@@ -41,10 +38,9 @@ export default function EachRoute() {
                 <Route path="Admin/Users" element={<AllUsers/>}/>
                 // UserProvider
                 <Route path="User/:id" element={<EachUserData/>}/>
-                <Route path="User/:id/Name" element={<UpdateUserName/>}/>
-                <Route path="User/:id/Email" element={<UpdateUserEmail/>}/>
+                <Route path="User/:id/Name" element={<EditUserName/>}/>
+                <Route path="User/:id/Email" element={<EditUserEmail/>}/>
             </Routes>
-            <Footer/>
         </BrowserRouter>
     )
 }
