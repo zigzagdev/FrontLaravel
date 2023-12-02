ec548dc596c0d6ac60f835f2e4b4c49380a93f27
zigzagdev <msts.oo0131@gmail.com>
2023-12-02 14:55:46 +0900

    SearchValidation-fix

--- src/frontend/src/components/app/input/Result.tsx
+++ src/frontend/src/components/app/input/Result.tsx
@@ -11,25 +11,8 @@ type searchResults = {
     categoryName: string
 }
 
-type searchQuery = {
-    total: number;
-    per: number;
-    onChange: (e: { page: number }) => void;
-}
-
-type pagination = {
-    total: number,
-    perPage: number,
-    currentPage: number,
-    from: number,
-    to: number,
-    lastPage: number
-}
-
-
 export function Result() {
     const queryParams = new URLSearchParams(window.location.search)
-    const [result, setResult] = useState(0);
     const query = queryParams.get("q")
     const [searchContents, setSearchContents] = useState<searchResults[]>([]);
     const baseURL = process.env.REACT_APP_API_BASE_URL;
@@ -39,8 +22,6 @@ export function Result() {
                 setSearchContents(res.data.data.itemInformation)
             })
     }, [])
-    const allResults = searchContents.length
-
     return (
         <>
             <div className="my-3 text-center block text-sm text-gray-300 sm:text-center
