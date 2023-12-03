cf20dc55fd68269f9c4fffe4ee1554495ba5c710
zigzagdev <msts.oo0131@gmail.com>
2023-12-03 16:09:51 +0900

    UserLoginForm-fix

--- src/frontend/src/components/app/auth/UserLogin.tsx
+++ src/frontend/src/components/app/auth/UserLogin.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {useForm, SubmitHandler} from "react-hook-form";
 import Footer from "../../common/footer/Footer";
 import Header from "../../common/header/Header";
@@ -50,52 +50,65 @@ export function UserLogin() {
     return (
         <>
             <Header/>
-            <div className="my-5 mx-36">
+            <div className="my-10 mx-24 flex justify-center">
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <h1 className="my-5">
                         <strong className="text-red-600">Login</strong>
                     </h1>
-                    <div className="">
-                        <label htmlFor="email" className="float-left mx-10">Email</label>
-                        <input
-                            autoComplete="email"
-                            type="email"
-                            className=""
-                            placeholder="Enter your mail"
-                            {...register("email", {required: true, minLength: 4})}
-                        />
-                        {errors.email?.type === "required" && (
-                            <span role="alert" className="text-red-400">Email is required</span>
-                        )}
-                        {errors.email && errors.email.type === "minLength" && (
-                            <span className="text-blue-700">Min length exceeded</span>
-                        )}
+                    <div className="my-6">
+                        <div className="my-2">
+                            <label htmlFor="email" className="">Email</label>
+                        </div>
+                        <div className="my-2">
+                            <input
+                                autoComplete="email"
+                                type="email"
+                                className="w-72 rounded-md h-8"
+                                placeholder="Enter your mail"
+                                {...register("email", {required: true, minLength: 4})}
+                            />
+                            {errors.email?.type === "required" && (
+                                <span role="alert" className="text-red-400">Email is required</span>
+                            )}
+                            {errors.email && errors.email.type === "minLength" && (
+                                <span className="text-blue-700">Min length exceeded</span>
+                            )}
+                        </div>
+                    </div>
+                    <div className="mt-9 mb-5">
+                        <div className="my-2">
+                            <label htmlFor="password">Password</label>
+                        </div>
+                        <div className="flex">
+                            <input
+                                autoComplete="password"
+                                {...register("password", {required: true, minLength: 8})}
+                                className="flex w-72 rounded-md h-8"
+                                placeholder="Enter your password"
+                                type={visiblePassword ? "text" : "password"}
+                            />
+                            <span className="mx-2.5" onClick={showPassword}>show password</span>
+                            {errors.password?.type === "required" && (
+                                <span role="alert" className="text-red-400">Password is required</span>
+                            )}
+                            {errors.password && errors.password.type === "minLength" && (
+                                <span className="text-blue-700">Min length exceeded</span>
+                            )}
+                        </div>
+                    </div>
+                    <div className="text-right mb-5 mt-10">
+                        <Link to= "" className="font-bold text-blue-500">
+                            Forgot password ?
+                        </Link>
                     </div>
                     <div className="">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            autoComplete="password"
-                            {...register("password", {required: true, minLength: 8})}
-                            className="flex"
-                            placeholder="Enter your password"
-                            type={visiblePassword ? "text" : "password"}
-                        />
-                        <span className="mx-1" onClick={showPassword}>show password</span>
-                        {errors.password?.type === "required" && (
-                            <span role="alert" className="text-red-400">Password is required</span>
-                        )}
-                        {errors.password && errors.password.type === "minLength" && (
-                            <span className="text-blue-700">Min length exceeded</span>
-                        )}
-                    </div>
-                    <p className="item">
                         <button
-                            className="btn btn-outline-primary text-center shadow-none mb-3"
+                            className="btn w-72 rounded-md h-8 bg-indigo-600"
                             type="submit"
                         >
                             Submit
                         </button>
-                    </p>
+                    </div>
                     {
                         <div className="my-5">
                             <span className="text-blue-400 text-lg">
