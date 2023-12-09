42d6a85a60a825c66594f8a2df9404a51ed715ea
zigzagdev <msts.oo0131@gmail.com>
2023-12-09 17:48:46 +0900

    RememberToken-add

--- src/backend/app/Services/RememberToken.php
+++ src/backend/app/Services/RememberToken.php
@@ -1,1 +1,30 @@
 <?php
+namespace App\Services;
+
+
+use App\Consts\Api\Number;
+use App\Models\Api\UserRememberToken;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+
+class RememberToken
+{
+    public static function generateRememberToken ($sendEmail)
+    {
+        $rememberToken = Str::random(20);
+
+        $rememberUserData = UserRememberToken::where('email', $sendEmail->email)->first();
+        if (!empty($rememberUserData)) {
+            $rememberUserData->delete();
+        }
+        var_dump($sendEmail->email);
+        $rememberUserData = UserRememberToken::create(
+            [
+                'remember_token' => $rememberToken,
+                'expire_at' => Carbon::now()->addHour(Number::One_Hour),
+                'email' => $sendEmail->email
+            ],
+        );
+        return $rememberUserData;
+    }
+}
