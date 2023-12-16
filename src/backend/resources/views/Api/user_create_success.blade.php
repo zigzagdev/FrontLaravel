6111c23755a00dc4ef73549d1323d8ec2f13a685
zigzagdev <msts.oo0131@gmail.com>
2023-12-16 19:26:07 +0900

    bladefile-fix

---
+++ src/backend/resources/views/Api/user_create_success.blade.php
@@ -0,0 +1,8 @@
+@extends('Common.master')
+@section('content')
+    Your account was created successfully !
+    Each data was registered with below data .
+
+
+
+@endsection
---
+++ src/backend/resources/views/Api/user_update_success.blade.php
@@ -0,0 +1,9 @@
+@extends('Common.master')
+@section('content')
+    Your data was updated successfully !
+    Each data was registered with below data .
+
+
+
+
+@endsection
