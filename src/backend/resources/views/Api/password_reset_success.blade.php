d9b160e0e0e95eebb9c535c9b3538b1936d83b34
zigzagdev <msts.oo0131@gmail.com>
2023-12-20 23:52:07 +0900

    fixMailMessage

--- src/backend/resources/views/Api/password_reset_success.blade.php
+++ src/backend/resources/views/Api/password_reset_success.blade.php
@@ -1,8 +1,13 @@
 @extends('Common.user-template')
 @section('content')
+
     Your password was updated successfully !
     Please log in app with this below URL
 
 
     {{$loginUrl}}
+
+    If you ignore this message, your password will
+    not be changed.
+
 @endsection
